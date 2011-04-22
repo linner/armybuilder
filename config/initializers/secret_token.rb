a09deac99e85e3fb8d814057e9f46d5b4a0abe32
Eric <linner@gmail.com>
2011-04-21 20:24:13 -0400

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArmyBuilder::Application.config.secret_token = 'f4d6f1bf52b4ef1264b9168370e033e4bbd2617b44d36fabf36d495691fe32dc839acb0aab8c3addd8b7584c117ccb1ab92cc234c25465c24fe7a6e11fd80cc3'
